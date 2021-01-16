cfbb89379a1b3584ee5412a1a44dfe40db0a84e4
a-bleier <bleier.adrian@gmx.de>
2021-01-16 15:31:45 +0100

    A birthday reminder will only be sent, when the date is correct.

--- main/cronJobs.go
+++ main/cronJobs.go
@@ -5,6 +5,8 @@ import (
 	"github.com/a-bleier/chagall_bot/comm"
 	"github.com/a-bleier/chagall_bot/db"
 	"github.com/robfig/cron"
+	"regexp"
+	"time"
 )
 
 //TODO: Write a cron job which will check everyday at 00:00 if someone needs to be remembered because of an birthday
@@ -15,7 +17,7 @@ func initCronJobs() {
 	cronObject.AddFunc("*/10 */1 * * * *", func() {
 		fmt.Println("Job done")
 	})
-	cronObject.AddFunc("*/30 */1 * * * *", checkBirthdays)
+	cronObject.AddFunc("*/10 */1 * * * *", checkBirthdays)
 	//For production
 	//cronObject.AddFunc("* * * */31 * *", checkBirthdays)
 	cronObject.Start()
@@ -33,6 +35,16 @@ func checkBirthdays() {
 	}
 
 	for _, entry := range ebrList {
+
+		t := time.Now()
+		_, month, day := t.Date()
+		pattern := fmt.Sprintf("(-|\\s)%02d(-|\\s)%02d", int(month), day)
+		fmt.Println("pattern ", pattern)
+		r, _ := regexp.Compile(pattern)
+		fmt.Println(entry.Date)
+		if r.MatchString(entry.Date) == false {
+			continue
+		}
 		responseString := fmt.Sprintf("Hi %s\n", entry.UserName)
 		responseString += fmt.Sprintf("%s has his / her birthday today.\n He / She was born on %s. You can reach this person in this way:\n %s", entry.Name, entry.Date, entry.Contact)
 		sMessage := comm.SendMessage{
